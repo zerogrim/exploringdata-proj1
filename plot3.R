579c5c77fefda767f0e513c216787ca95a45de48
zerogrim <lowell@toshi.(none)>
2014-06-08 17:48:57 -0400

    added legends

--- plot3.R
+++ plot3.R
@@ -29,4 +29,8 @@ points( data$Date,
         type="l", 
         col="Blue")        
 
+legend( "topright", 
+        lty=c(1,1),
+        col= c("Black","Red","Blue"),
+        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3") )
 dev.off()
--- plot4.R
+++ plot4.R
@@ -55,7 +55,10 @@ points( data$Date,
         col="Blue")        
 
 
-
+legend( "topright", 
+        lty=c(1,1),
+        col= c("Black","Red","Blue"),
+        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3") )
 
 ### PLOT 3 ######
 
